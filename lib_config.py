0943f9348791f3bb66f13f4a77050cef93f42ee5
Gunars Frickauss <gunarsf@gmail.com>
2024-10-01 15:59:25 +0300

    Add files via upload

---
+++ lib_config.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+import configparser
+import logging
+import os
+
+def read_configuration(config_file:str) -> dict:
+    '''
+    Reads configuration file, sets default values if missing
+    
+    Args:
+        configuration file
+
+    Returns dict with configuration settings
+    '''
+    # Default config values
+    
+    bcr_config = {
+        'dept_name': 'Vide',
+        'log_file': None,
+        'label_template_file': 'label_template.txt',
+        'label_data_file': 'vide.txt',
+        'label_number_of_copies': 1,
+        'bc_port': '/dev/ttyACM0',
+        'bc_reader_timeout': 1,                                                                                                
+        'bc_reader_debounce': 1.5,
+        'bc_reader_reset': 60,
+        'include_schemes': ['usb'],
+        'driver_list': {'Zebra':'Raw'},
+        'barcode_regex': '^#\d{7,9}$',
+        }
+    
+    if not os.path.isfile(config_file):
+        logging.critical("Config file {} does not exist!".format(config_file))
+        return(None)
+    config = configparser.ConfigParser(allow_no_value=True,
+                        converters={'list': lambda x: [int(i.strip()) for i in x.split(',')],
+                                    'list_s' : lambda x: [i.strip() for i in x.split(',')]})
+    config.read(config_file)
+    
+    #config = default_config
+    #Get values from ini file line by line to catch errors in config file
+    commands = (
+        "bcr_config['log_file'] = config.get('LOGGING','log_file')",
+        "bcr_config['dept_name'] = config.get('INFO','dept_name')",
+        "bcr_config['label_template_file'] = config.get('LABEL','label_template_file')",
+        "bcr_config['label_data_file'] = config.get('LABEL','label_data_file')",
+        "bcr_config['label_number_of_copies'] = config.getint('LABEL','label_number_of_copies')",
+        "bcr_config['bc_reader_port'] = config.get('BARCODE','bc_reader_port')",
+        "bcr_config['bc_reader_timeout'] = config.getfloat('BARCODE','bc_reader_timeout')",
+        "bcr_config['bc_reader_debounce'] = config.getfloat('BARCODE','bc_reader_debounce')",
+        "bcr_config['bc_regex'] = config.get('BARCODE','bc_regex')",
+        "bcr_config['prn_driver_list'] = config.getlist_s('PRINTER','prn_driver_list')",
+        "bcr_config['prn_include_schemes'] = config.getlist_s('PRINTER','prn_include_schemes')",
+    )
+
+    for command in commands:
+        try:
+            exec(command)
+        except Exception as e:
+            logging.error(e)
+    return(bcr_config)
+
+def main():
+    config_file = 'bcp.ini'
+    run_directory= os.path.dirname(os.path.realpath(__file__))
+    full_path = '{}/{}'.format(run_directory,config_file)
+    c = read_configuration(full_path)
+    c=print(c)
+
+if __name__ == '__main__':
+    main()
